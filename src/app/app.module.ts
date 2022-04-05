8202c805b6f5a361ce4c48169bad8ad5fa25ed9d
Dilan <dilan.roessell@gmail.com>
2022-04-05 18:10:58 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
+import { MissionlistComponent } from './missionlist/missionlist.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MissiondetailsComponent,
+    MissionlistComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatCardModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/mission.ts
@@ -0,0 +1,9 @@
+export interface Mission {
+    flight_number:string;
+    mission_name: string;
+    launch_year: string;
+    details: string;
+    rocket: any;
+    launch_site: any;
+    links: any;
+}
